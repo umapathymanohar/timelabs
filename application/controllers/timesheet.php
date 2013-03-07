ab33ace749fbebd07fb9b9dd97f9375618cd6b03
Umapathy Manohar <umapathymanohar@gmail.com>
2013-03-07 17:00:35 +0530

    Update Button

--- application/controllers/timesheet.php
+++ application/controllers/timesheet.php
@@ -2,39 +2,55 @@
 
 class Timesheet_Controller extends Base_Controller {
 
-	/*
-	|--------------------------------------------------------------------------
-	| The Default Controller
-	|--------------------------------------------------------------------------
-	|
-	| Instead of using RESTful routes and anonymous functions, you might wish
-	| to use controllers to organize your application API. You'll love them.
-	|
-	| This controller responds to URIs beginning with "home", and it also
-	| serves as the default controller for the application, meaning it
-	| handles requests to the root of the application.
-	|
-	| You can respond to GET requests to "/home/profile" like so:
-	|
-	|		public function action_profile()
-	|		{
-	|			return "This is your profile!";
-	|		}
-	|
-	| Any extra segments are passed to the method as parameters:
-	|
-	|		public function action_profile($id)
-	|		{
-	|			return "This is the profile for user {$id}.";
-	|		}
-	|
-	*/
-
 	public function action_index()
 	{
 		$timesheet = Auth::user()->timesheet()->order_by('id', 'desc')->get();
         return View::make('timesheet.index', array('timesheet' => $timesheet));
-;
+
+
 	}
+
+	public function action_checkin()
+    {
+        $logged_in_user = Auth::user();
+        
+        
+        $data = array(
+            array(
+                'user_id' => $logged_in_user->id,
+
+                'check_in_date' => date('Y-m-d'),
+                'check_in_time' => date('H:i:s'),
+
+            )
+        );
+        $logged_in_user->timesheet()->save($data);
+        return Redirect::to('timesheet');
+    }
+
+
+    public function action_checkout()
+    {
+    $logged_in_user = Auth::user();
+   $qry=  Timesheet::where('user_id', '=', $logged_in_user->id)->where('check_in_date', '=', '2013-03-07')->get();
+
+foreach ($qry as $data)
+{
+     $update_id = $data->id;
+     $in_time = $data->check_in_time;
+}
+
+$post = Timesheet::find($update_id);
+
+$post->check_out_time = date('H:i:s');
+
+
+
+
+$post->save();
+
+    
+return Redirect::to('timesheet');
+    }
 
 }
--- application/views/timesheet/index.blade.php
+++ application/views/timesheet/index.blade.php
@@ -14,21 +14,30 @@
             <td>Check In</td>
             <td>Check Out</td>
             <td>Working Hours</td>
-            <td>Overtime</td>
+
             <tbody>
  
       
             @forelse ($timesheet as $time)
+            <tr>
                 <td>{{ $time->check_in_date}}</td>
                 <td>{{ $time->check_in_time}}</td>
                 <td>{{ $time->check_out_time}}</td>
-                <td></td>
-                <td></td>
+
+                <?php 
+                    $working_hours = strtotime($time->check_out_time) - strtotime($time->check_in_time);
+
+
+                    
+                    $working_hours =  date("H:i" , mktime(0,0,$working_hours));
+                ?>
+                <td><?php echo $working_hours ?> hours</td>
             @empty
             <div class="alert alert-info">
             <h4 class="alert-heading">Awww!</h4>
             <p>Seems like you don't have any entry. <a href="#">New to Timelabs?</a></p>
             </div>
+            </tr>
           @endforelse
             </tbody>
         </thead>
@@ -40,11 +49,15 @@
     <div class="span3">
 
     <div class="well">
-        <button class="btn">Check in</button>
+
+    <form id = "checkin"class="well" method="POST" action="timesheet/checkin">
+        <button class="btn"  >Check in</button>
+    </form>
+    <form class="well" method="POST" action="timesheet/checkout">
         <button class="btn">Check out</button>
+    </form>
     </div>
- 
-    </div>
+ </div>
     
 </div>
 
